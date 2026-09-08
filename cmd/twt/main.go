66673c0b76cbebbf93315ce67a4fdb2aa9f2c8aa
Andrew Chen <andrewkkchen@gmail.com>
2026-09-08 14:42:08 -0700

    rename search command dependency

--- cmd/twt/main.go
+++ cmd/twt/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"os"
 
-	searchdependency "github.com/ach968/x-twt-cli/cmd/twt/dependencies/commands/search"
+	searchcommanddependency "github.com/ach968/x-twt-cli/cmd/twt/dependencies/commands/search"
 	"github.com/ach968/x-twt-cli/internal/app/cli"
 )
 
 func main() {
-	application := cli.New(cli.Dependencies{Search: searchdependency.New()})
+	application := cli.New(cli.Dependencies{Search: searchcommanddependency.New()})
 	status := application.Run(context.Background(), os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
 	os.Exit(status)
 }
