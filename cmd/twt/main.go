87a236141ef4112e1a23a15193f32e238430f281
Andrew Chen <andrewkkchen@gmail.com>
2026-09-16 09:59:37 -0700

    rename dependency->command

--- cmd/twt/main.go
+++ cmd/twt/main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"runtime/debug"
 
-	bookmarkscommanddependency "github.com/ach968/x-twitter-cli/cmd/twt/dependencies/commands/bookmarks"
-	searchcommanddependency "github.com/ach968/x-twitter-cli/cmd/twt/dependencies/commands/search"
+	bookmarkscommand "github.com/ach968/x-twitter-cli/cmd/twt/dependencies/commands/bookmarks"
+	searchcommand "github.com/ach968/x-twitter-cli/cmd/twt/dependencies/commands/search"
 	"github.com/ach968/x-twitter-cli/internal/app/cli"
 	"github.com/ach968/x-twitter-cli/internal/app/cli/dependencies/commands"
 	"github.com/ach968/x-twitter-cli/internal/app/management"
@@ -29,7 +29,7 @@ func main() {
 	if err != nil {
 		os.Exit(commands.WriteFailure(os.Stderr, "LOCAL_STATE_UNAVAILABLE", "Unable to resolve local application state"))
 	}
-	application := cli.New(cli.Dependencies{Search: searchcommanddependency.New(), Bookmarks: bookmarkscommanddependency.New(), Management: managementService, Version: installedVersion()})
+	application := cli.New(cli.Dependencies{Search: searchcommand.New(), Bookmarks: bookmarkscommand.New(), Management: managementService, Version: installedVersion()})
 	status := application.Run(context.Background(), os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
 	os.Exit(status)
 }
